c61cff2f880a4e64846cecd4b85421ffd3f16834
Sebu-Dev <fsebulke@gmail.com>
2024-12-05 16:13:50 +0100

    Add motion effects to portfolio main page, start using images - Implemented motion effects using Framer Motion for enhanced interactivity. - Integrated images into the portfolio main page for better visual appeal. - Updated layout to accommodate images alongside text content. - Improved UI with hover effects and animations for a more dynamic user experience.

---
+++ portfolio/src/components/Technologies.tsx
@@ -0,0 +1,90 @@
+import { motion, type Variants } from "framer-motion";
+import { FaDatabase, FaJava } from "react-icons/fa";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiBootstrap, SiSpring, SiTailwindcss, SiTypescript } from "react-icons/si";
+
+
+const iconVariants = (duration: number): Variants => ({
+
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        },
+    },
+});
+
+function Technologies() {
+    return (
+        <div className='border-b border-neutral-800 pb-24'>
+            <motion.h2
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1.5 }}
+                className=' my-20 text-center text-4xl'>Technologies </motion.h2>
+            <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <RiReactjsLine className="text-7xl text-cyan-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaJava className="text-7xl text-amber-600	" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiTypescript className="text-7xl text-blue-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiSpring className="text-7xl text-lime-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(6)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiBootstrap className="text-7xl text-purple-500" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <SiTailwindcss className="text-7xl text-sky-400" />
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(7)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
+                    <FaDatabase className="text-7xl text-red-900" />
+                </motion.div>
+
+
+            </motion.div>
+        </div>
+    )
+}
+
+export default Technologies
