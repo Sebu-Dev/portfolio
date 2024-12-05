7b29e4f9ceef74ee70ab66a31f851340918d6e6e
Florian_Sebulke <florian.sebulke@volkswagen.de>
2024-12-05 20:09:43 +0100

    Edit Navbar Styling added some motion for the icons added motion for scroll to contacts

--- portfolio/src/components/Contact.tsx
+++ portfolio/src/components/Contact.tsx
@@ -1,9 +1,11 @@
 import { motion } from "motion/react";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
 import { CONTACT } from "../constants/index.tsx";
 
 function Contact() {
     return (
-        <div className='border-b border-neutral-900 pb-20'>
+        <div id="contact" className='border-b border-neutral-900 pb-20'>
             <motion.h2
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: -100 }}
@@ -27,5 +29,32 @@ function Contact() {
         </div>
     )
 }
+function MailLink() {
+    return (
+        <motion.a
+            href="https://github.com/Sebu-Dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative inline-block p-4 text-4xl text-black hover:text-white transition"
+            whileHover={{
+                scale: 1.2,
+                rotate: 10,
+            }}
+            whileTap={{
+                scale: 0.9,
+            }}
+        >
+            <FaGithub />
+            <motion.div
+                className="absolute inset-0 -z-10 rounded-full bg-purple-400 blur-lg opacity-50"
+                whileHover={{
+                    scale: 1,
+                    opacity: 0.5,
+                }}
+            />
+            <AiOutlineMail />
 
+        </motion.a>
+    );
+}
 export default Contact
